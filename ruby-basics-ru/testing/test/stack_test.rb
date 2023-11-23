560332d8a99cf3b04a1578d22661a929983c4829
@hexlet/cli <support@hexlet.io>
2023-11-23 18:49:47 +0600

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -4,8 +4,40 @@ require_relative 'test_helper'
 require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
+  def setup; end
+
+  def teardown; end
+
   # BEGIN
-  
+  def test_stack_should_be_empty
+    stack = Stack.new
+    assert { stack.to_a.empty? == true }
+  end
+
+  def test_stack_adding_element
+    push_element = 10
+    stack = Stack.new
+    stack.push! push_element
+    assert {stack.to_a[0] == push_element}
+  end
+
+  def test_stack_remove_element
+    push_element = 10
+    stack = Stack.new
+    stack.push! push_element
+    stack.pop!
+    assert {stack.to_a.empty? == true}
+  end
+
+  def test_stack_clear_all
+    stack = Stack.new
+    stack.push! 10
+    stack.push! 20
+    stack.push! 30
+    stack.push! 40
+    stack.clear!
+    assert {stack.to_a.empty? == true}
+  end
   # END
 end
 
